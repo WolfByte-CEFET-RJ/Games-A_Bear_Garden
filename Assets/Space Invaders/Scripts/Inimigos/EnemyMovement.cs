f6c443e28218bfec2334deff3004d65b3b88b180
grocre <grocre@gmail.com>
2022-01-25 10:14:05 -0300

    Alguns Debug.logs deletados e comentários adicionados

--- Assets/Space Invaders/Scripts/Inimigos/EnemyMovement.cs
+++ Assets/Space Invaders/Scripts/Inimigos/EnemyMovement.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-	public float enemyMovementSpeed = 140;
-	public float enemyTimer = 1;
-	public int attemptingMoves = 6;
-	private float enemyMovementTimer;
-	private int timesEnemyMoved = 0;
-	private int timesEnemyPassedThroughScreen = 0;
-	private Vector3 direction;
+	public float enemyMovementSpeed = 140; // deslocamento do inimigo
+	public float enemyTimer = 1; // time que o inimigo vai levar para se movimentar
+	public int attemptingMoves = 6; // quantas vezes ele vai se mover antes de mudar de lado
+	private float enemyMovementTimer; // variável que vai marcar o tempo
+	private int timesEnemyMoved = 0; //vezes que o inimigo se moveu
+	private int timesEnemyPassedThroughScreen = 0; //número de vezes que eles passaram pela tela
+	private Vector3 direction; //vetor de atribuição da direção
 
 	void Update()
 	{
@@ -29,24 +29,26 @@ public class EnemyMovement : MonoBehaviour
 		// atribuição do tempo
 		enemyMovementTimer += Time.deltaTime;
 
-		attemptingMoves = timesEnemyPassedThroughScreen == 0 ? 6: 6*2;
+		// numero de movimentos recebe 6 se o a variável de número de vezes que passou na tela for 0 e 12 se for em qualquer outra vez
+		attemptingMoves = timesEnemyPassedThroughScreen == 0 ? 6: 6*2; // pensar em uma maneira de deixar menos hard coded
 
-		if (timesEnemyMoved == attemptingMoves)
+		if (timesEnemyMoved == attemptingMoves) // se o número de vezes que ele se mexeu for igual ao número máximo que esperamos
 		{
-			timesEnemyPassedThroughScreen++;
-			timesEnemyMoved = 0;
+			timesEnemyPassedThroughScreen++; // numero de vezes que ele passou pela tela recebe +1 
+			timesEnemyMoved = 0; // e o número de vezes que se moveu vira 0
 		}
 
-		direction = timesEnemyPassedThroughScreen % 2 == 0 ? Vector3.right : Vector3.left; // ainda não tá 100%
+		// se o número de vezes que passou pela tela tiver mod 2 == 0 recebe movimento para a direita
+		// senão movimento para a esquerda
+		direction = timesEnemyPassedThroughScreen % 2 == 0 ? Vector3.right : Vector3.left; 
 
 		//movimento coordenado do inimigo -> de 1 em 1s o inimigo se mexe
 		if (enemyMovementTimer >= enemyTimer)
 		{
+			// movimentação do inimigo
 			transform.Translate(direction * enemyMovementSpeed * Time.deltaTime);
-			timesEnemyMoved++;
-			enemyMovementTimer = 0;
-			Debug.Log(timesEnemyMoved);
-			Debug.Log(attemptingMoves);
+			timesEnemyMoved++; // toda vez que ele se move a variável aumenta 1
+			enemyMovementTimer = 0; //e o timer 0
 		}
 	}
 }
