e15c435e3318954576c088c384638fa8021bb118
grocre <grocre@gmail.com>
2022-01-25 10:01:05 -0300

    Movimento direita e esquerda funcionando
    Coloquei de uma maneira que consiga funcionar, porém, ainda tá hard coded

--- Assets/Space Invaders/Scripts/Inimigos/EnemyMovement.cs
+++ Assets/Space Invaders/Scripts/Inimigos/EnemyMovement.cs
@@ -4,45 +4,50 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public float enemyMovementSpeed = 140;
-    public float enemyTimer = 1;
-    public float enemyMovementTimer;
-    private float timesEnemyMoved = 0;
-    private float timesEnemyPassedThroughScreen = 0;
+	public float enemyMovementSpeed = 140;
+	public float enemyTimer = 1;
+	public int attemptingMoves = 6;
+	private float enemyMovementTimer;
+	private int timesEnemyMoved = 0;
+	private int timesEnemyPassedThroughScreen = 0;
+	private Vector3 direction;
 
-    void Update()
-    {
-        enemyMovement();
-    }
+	void Update()
+	{
+		enemyMovement();
+	}
 
-    //Tiro -> destroi inimigo
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Tiro")
-            Destroy(this.gameObject);
-    }
+	//Tiro -> destroi inimigo
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Tiro")
+			Destroy(this.gameObject);
+	}
 
-    void enemyMovement()
-    {
-        // atribuição do tempo
-        enemyMovementTimer += Time.deltaTime;
-        Vector3 direction;
+	void enemyMovement()
+	{
+		// atribuição do tempo
+		enemyMovementTimer += Time.deltaTime;
 
+		attemptingMoves = timesEnemyPassedThroughScreen == 0 ? 6: 6*2;
 
-        if (timesEnemyMoved >= 6)
-        {
-            timesEnemyPassedThroughScreen++;
-        }
+		if (timesEnemyMoved == attemptingMoves)
+		{
+			timesEnemyPassedThroughScreen++;
+			timesEnemyMoved = 0;
+		}
 
-        direction = timesEnemyPassedThroughScreen % 2 == 0 ? Vector3.right : Vector3.left; // ainda não tá 100%
-        
-        //movimento coordenado do inimigo -> de 1 em 1s o inimigo se mexe
-        if (enemyMovementTimer > enemyTimer)
-            {
-                transform.Translate(direction * enemyMovementSpeed * Time.deltaTime);
-                timesEnemyMoved++;
-                enemyMovementTimer = 0; 
-            }
-    }
+		direction = timesEnemyPassedThroughScreen % 2 == 0 ? Vector3.right : Vector3.left; // ainda não tá 100%
+
+		//movimento coordenado do inimigo -> de 1 em 1s o inimigo se mexe
+		if (enemyMovementTimer >= enemyTimer)
+		{
+			transform.Translate(direction * enemyMovementSpeed * Time.deltaTime);
+			timesEnemyMoved++;
+			enemyMovementTimer = 0;
+			Debug.Log(timesEnemyMoved);
+			Debug.Log(attemptingMoves);
+		}
+	}
 }
 
