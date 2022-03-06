5e3b9a5a0950ed3ff913ac240827ed4bc15fc6c4
Natália Santos <91389897+QueenNatty@users.noreply.github.com>
2022-03-06 03:00:33 -0300

    Volume 1000%
    Volumes separados e salvando os valores para próxima fase

--- Assets/Menu/Scripts/Volume.cs
+++ Assets/Menu/Scripts/Volume.cs
@@ -2,14 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Volume : MonoBehaviour
 {
-    public Slider volumeSlider;
+    [SerializeField] AudioMixer _mixer;
+    [SerializeField] Slider musicaSlider;
+    [SerializeField] Slider sfxSlider;
+    [SerializeField] Slider masterSlider;
 
-    public void alterarVolume()
+    public const string Mixer_Musica = "MusicaVolume";
+    public const string Mixer_SFX    = "SFXVolume";
+    public const string Mixer_Master = "MasterVolume";
+
+    private void Awake()
     {
-        AudioListener.volume = volumeSlider.value;
-        Debug.Log("Volume = " + volumeSlider.value);
+        musicaSlider.onValueChanged.AddListener(SetMusicaVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+    }
+
+    private void Start()
+    {
+        musicaSlider.value = PlayerPrefs.GetFloat(AudioManager.Musica_Chave, 1f);
+        sfxSlider.value    = PlayerPrefs.GetFloat(AudioManager.SFX_Chave, 1f);
+        masterSlider.value = PlayerPrefs.GetFloat(AudioManager.Master_Chave, 1f);
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(AudioManager.Musica_Chave, musicaSlider.value);
+        PlayerPrefs.SetFloat(AudioManager.SFX_Chave, sfxSlider.value);
+        PlayerPrefs.SetFloat(AudioManager.Master_Chave, masterSlider.value);
+    }
+
+    void SetMusicaVolume(float value)
+    {
+        _mixer.SetFloat(Mixer_Musica, Mathf.Log10(value) * 20);
+    }
+
+    void SetSFXVolume(float value)
+    {
+        _mixer.SetFloat(Mixer_SFX, Mathf.Log10(value) * 20);
+    }
+
+    void SetMasterVolume(float value)
+    {
+        _mixer.SetFloat(Mixer_Master, Mathf.Log10(value) * 20);
     }
 }
