5206c91d9938142cf70f48abebe26afdd3190f81
Rodriggs <rodrigo.cavalcante@aluno.cefet-rj.br>
2022-03-23 16:41:01 -0300

    Scripts base prontos
    Com esse commit, ficam prontos os scripts de: movimentação das tropas, spawn das tropas temporizado, colisão das tropas. Todos aplicáveis tanto às normais quanto supertropas com exceção da colisão que será explorada com vidas no futuro.

--- Assets/Jokenpo/Scripts/Tropas/TropasSpawn.cs
+++ Assets/Jokenpo/Scripts/Tropas/TropasSpawn.cs
@@ -5,13 +5,36 @@ using UnityEngine;
 public class TropasSpawn : MonoBehaviour
 {
     //Script em andamento, não faz nada por agora (nem tem nada mesmo :P)
+
+    public GameObject tropaQualquer;
+    private int tempoInt, j;
+    private float temporizador;
+
     void Start()
     {
-
+        //Instantiate(tropaQualquer, transform.position, transform.rotation);
+        temporizador = 0;
+        j = 0;
     }
     
     void Update()
     {
-        
+        //TEMPORIZADOR
+        temporizador += Time.deltaTime;
+
+        tempoInt = (int)temporizador;
+
+        if(tempoInt % 2 == 0)
+        {
+            for(int i = j; j < 1; i++)
+            {
+                Instantiate(tropaQualquer, transform.position, transform.rotation);
+                j = 1;
+            }
+        }
+        else
+        {
+            j = 0;
+        }
     }
 }
