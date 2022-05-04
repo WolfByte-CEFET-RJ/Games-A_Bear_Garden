36c14508c30f6b764b7c327c248b1cc8c7a3f575
Vinicius Santos <viniciusantos0898@gmail.com>
2022-05-03 22:12:04 -0300

    Adicionados inputs para os 4 tipos de players

--- Assets/Space Invaders/Scripts/Player/PlayerInput.cs
+++ Assets/Space Invaders/Scripts/Player/PlayerInput.cs
@@ -4,11 +4,45 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    protected float horizontalI;
-    protected float verticalI;
+    public enum PlayerType { PLAYER1, PLAYER2, PLAYER3, PLAYER4 }
     
+    [SerializeField] protected float horizontalI;
+    [SerializeField] protected float verticalI;
+    
+    [SerializeField] private PlayerType tipo;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        GetPlayerInput(tipo);
+    }
+    private void GetPlayerInput(PlayerType type)
+    {
+        switch (type)
+        {
+            case PlayerType.PLAYER1:
+                horizontalI = Input.GetAxis("Horizontal");
+                break;
+            case PlayerType.PLAYER2:
+                horizontalI = Input.GetAxis("Horizontal2");
+                break;
+            case PlayerType.PLAYER3:
+                horizontalI = Input.GetAxis("Horizontal3");
+                break;
+            case PlayerType.PLAYER4:
+                horizontalI = Input.GetAxis("Horizontal4");
+                break;
+            default: horizontalI = verticalI = 0;
+                break;
+
+        }
+    }
+
     public Vector2 GetInput()
     {
-        return new Vector2(horizontalI = Input.GetAxis("Horizontal"), verticalI = Input.GetAxis("Vertical"));
+        return new Vector2(horizontalI, 0);
     }
 }
