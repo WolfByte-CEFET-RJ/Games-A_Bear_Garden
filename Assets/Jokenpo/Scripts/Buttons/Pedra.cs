d55798c8352e09f4990dd107a2256c3090bb6c0b
Nulo0 <Danielpedro7676@gmail.com>
2022-05-08 21:45:25 -0300

    Mostragem do resultado da rodada na tela
    Agora o resultado das rodadas do Jokenpo são mostrados na tela e não só no console. Houve mudança no nome do Text "vidaVilao" para "resultado_papel", e foram criados 2 outros Texts para a pedra e a tesoura.

--- Assets/Jokenpo/Scripts/Buttons/Pedra.cs
+++ Assets/Jokenpo/Scripts/Buttons/Pedra.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Pedra : MonoBehaviour
 {
+    public Text resultado_pedra; //Daniel --> Variável para manipular a UI do resultado do jokenpo referente a pedra.
     public static int verificacao_pedra;  //Daniel --> Variável que controla spawn de tropas de pedra
     int verificacao;
     public TempoJokenpo tempJkp;    //Rodrigo --> Variável que instancia a classe controladora do tempo "TempoJokenpo"
@@ -49,7 +51,8 @@ public class Pedra : MonoBehaviour
    }
 
     // ANALISA OS RESULTADOS DO PLAYER E DO VILÃO
-    int checkResultados(int v){    
+    int checkResultados(int v){   
+        string situacao; //Daniel --> Variável para armazenar situacao da partida a ser usada na corrotina.
        
         /*Foi utilizado o sistema abaixo ao longo de todo o jogo, ou seja,
         esses códigos de verificação serão iguais para todos os scripts*/
@@ -57,18 +60,32 @@ public class Pedra : MonoBehaviour
        if(v == 3){
            Debug.Log("Situacao: Vitoria Vilao!");
            verificacao = 2;
+           situacao = "Vitoria Vilao!";
+           StartCoroutine(result(situacao)); //Daniel --> Iniciando corrotina.
 
        } else if(v == 2){
            Debug.Log("Situacao: Vitoria Player!");
            verificacao = 1;
+           situacao = "Vitoria Player!";
+           StartCoroutine(result(situacao));
 
        } else{
            Debug.Log("Situacao: Empate!");
            verificacao = 0;
+           situacao = "Empate!";
+           StartCoroutine(result(situacao));
 
        }
 
         return verificacao;
    }
+
+   IEnumerator result(string situacao)
+    {
+        //Daniel --> Corrotina para mostrar a situação na tela e depois de 2 segundos limpar.
+        resultado_pedra.text = situacao;
+        yield return new WaitForSeconds(2f); //Altere esse valor para mudar os segundos.
+        resultado_pedra.text = "";
+    }
 
 }
