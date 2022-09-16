3fe7097685bc58a93d9549b3652827d552fc4c5a
JoelRTavares <108430269+JoelRTavares@users.noreply.github.com>
2022-09-16 11:45:20 -0300

    Inputs de tiro diferentes
    Agora cada um dos 4 Players possuem um botão de tiro diferente
    P1: Seta pra Cima
    P2: W
    P3: T
    P4: I

--- Assets/Space Invaders/Scripts/Player/PlayerInput.cs
+++ Assets/Space Invaders/Scripts/Player/PlayerInput.cs
@@ -15,17 +15,32 @@ public abstract class PlayerInput : MonoBehaviour
         switch (type)
         {
             case PlayerType.PLAYER1:
-                return  Input.GetAxis("Horizontal");
+                return  Input.GetAxis("Horizontal");//Setas pra direita e esquerda
             case PlayerType.PLAYER2:
-                return  Input.GetAxis("Horizontal2");
+                return  Input.GetAxis("Horizontal2");//A e D
             case PlayerType.PLAYER3:
-                return  Input.GetAxis("Horizontal3");
+                return  Input.GetAxis("Horizontal3");//F e H
             case PlayerType.PLAYER4:
-                return  Input.GetAxis("Horizontal4");
+                return  Input.GetAxis("Horizontal4");//J e L
             default: return  0;
 
         }
     }
-
+    protected string GetShootInput(PlayerType type)//Metodo para retornar o nome do Input de tiro (Adicionados no Input manager)
+    {
+        switch (type)
+        {
+            case PlayerType.PLAYER1:
+                return "ShootSI1";//Seta pra cima
+            case PlayerType.PLAYER2:
+                return "ShootSI2";//W
+            case PlayerType.PLAYER3:
+                return "ShootSI3";//t
+            case PlayerType.PLAYER4:
+                return "ShootSI4";//i
+            default: Debug.LogError("Input não encontrado");
+                return "null";
+        }
+    }
  
 }
--- Assets/Space Invaders/Scripts/Shoot/Weapon.cs
+++ Assets/Space Invaders/Scripts/Shoot/Weapon.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public class Weapon : PlayerInput
 {
 
     public Tirinho shootObject; //prefab = tirinho
     public Transform firePoint;
+    private string InputName;
+    private void Start()
+    {
+        InputName = GetShootInput(playerType);//Referenciar o nome do Input pelo metodo na classe pai
+    }//Mudanca feito pelo fato de que antes, os 4 players usariam o mesmo botao pra atirar
 
-    // Update is called once per frame
     void Update()
     {
         //queria deixar o espaço como trigger 
         //Esse comando também acaba com o problema dos tiros estarem se multiplicando
-		if (Input.GetButtonDown("Jump"))
+		if (Input.GetButtonDown(InputName))
 		{
             Shoot();
 		}
