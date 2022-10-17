c9180ad689e2b555a8cd60c22c58048f1bf0bd5a
JoelRTavares <108430269+JoelRTavares@users.noreply.github.com>
2022-10-17 20:16:10 -0300

    Feedback visual para criação de waves

--- Assets/Space Invaders/Scripts/Canvas/CanvasController.cs
+++ Assets/Space Invaders/Scripts/Canvas/CanvasController.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class CanvasController : MonoBehaviour
 {
-  
+    [SerializeField] private Image waveTimer;
+
+    public delegate void EnemyWaveUI(float currentTime, float maxTime);
+    public static EnemyWaveUI EnemyWave;
+
+    private void Start()
+    {
+        EnemyWave = SetWaveUI;
+    }
+
+    void SetWaveUI(float time, float maxTime)
+    {
+        waveTimer.fillAmount = time / maxTime;
+    }
     public void ToScene(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+    private void OnDisable()
+    {
+        EnemyWave -= SetWaveUI;
     }
 }
--- Assets/Space Invaders/Scripts/Inimigos/EnemySpawner.cs
+++ Assets/Space Invaders/Scripts/Inimigos/EnemySpawner.cs
@@ -7,7 +7,10 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float timeToSpawn;
     [SerializeField] private GameObject wave;
 
+    [SerializeField] private Transform[] Effects;
+
     private float cronometer;
+    private bool effectCreated = false;
     void Start()
     {
         
@@ -17,10 +20,31 @@ public class EnemySpawner : MonoBehaviour
     void Update()
     {
         cronometer += Time.deltaTime;
+        if(cronometer >= timeToSpawn - 0.5f && !effectCreated)
+        {
+            CreateEffect();
+            effectCreated = true;
+        }
         if(cronometer >= timeToSpawn)
         {
             Instantiate(wave, transform.position, transform.rotation);
             cronometer = 0f;
+            effectCreated = false;
+        }
+
+        if(CanvasController.EnemyWave != null)
+        {
+            CanvasController.EnemyWave(timeToSpawn - cronometer, timeToSpawn);
+        }
+        for (int i = 0; i < Effects.Length; i++)
+            Effects[i].Rotate(0f, 0f, 1f, Space.World);
+    }
+
+    void CreateEffect()
+    {
+        for (int i = 0; i < Effects.Length; i++)
+        {
+            Effects[i].GetComponent<Animator>().Play("SpawnEffect");           
         }
     }
 }
