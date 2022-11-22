b97936351790337a364121462691908880a6b5d9
Rodriggs <rodrigo.cavalcante@aluno.cefet-rj.br>
2022-11-22 01:01:30 -0300

    Reconstruindo Labirinto
    Enviando scripts modificados UM commit após o meu de 19 de Outubro

--- Assets/Labirinto/Scripts/Maquina de Estados/MoveSelectionState.cs
+++ Assets/Labirinto/Scripts/Maquina de Estados/MoveSelectionState.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class MoveSelectionState : State
 {
+   // ESTA VARIAVEL EnableSpawn E A CONDICAO DO OnFire ESTARÁ NESSE CODIGO TEMPORARIAMENTE ATÉ SER CRIADO UM ESTADO SOMENTE PARA SPAWNAR TRAP, QUE POSSA SER SELECIONADO NA HOLD BAR, SO PECO PARA NAO APAGAREM, DEIXEM COMENTADO TMJ!
+   // public static bool EnableSpawn{get; set;} // Eduardo --> Variavel que irá permitir a spawn da trap ou não
     public override void Enter()
    {
       base.Enter();
+      //EnableSpawn = true;
       inputs.OnMove+=OnMoveTileSelector;
       inputs.OnFire+=OnFire;
    }
@@ -22,7 +25,10 @@ public class MoveSelectionState : State
       if(button==1)
       {
         machine.ChangeTo<MoveSequenceState>();
-        //machine.ChangeTo<TurnoDoVilao>();
+        /*if(Selector.instance.spriteRenderer.sortingOrder == 300 && EnableSpawn == true) // Eduardo --> Se o selector está na ordem de renderizacao do andar 01 e se o seletor nao estiver colidindo com uma trap, é possivel inicializar o turno de spawnar trap na posicao onde o selector está, esse código esta alocado temporariamente 
+        {
+           machine.ChangeTo<TurnoDoVilao>();
+        }*/
       }
       else if(button==2)
       {
--- Assets/Labirinto/Scripts/Tabuleiro/Selector.cs
+++ Assets/Labirinto/Scripts/Tabuleiro/Selector.cs
@@ -15,4 +15,28 @@ public class Selector : MonoBehaviour
         instance = this;
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
+
+        private void OnCollisionEnter2D(Collision2D col) {
+        if(col.gameObject.tag == "TrapVilao")
+        {
+            Debug.Log("O selector entrou em colisao com a trapVIlao");
+           // MoveSelectionState.EnableSpawn = false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D col) {
+        if(col.gameObject.tag == "TrapVilao")
+        {
+            Debug.Log("O selector está colidindo com a trapVilao");
+           // MoveSelectionState.EnableSpawn = false;
+        }
+    }
+
+     private void OnCollisionExit2D(Collision2D col) {
+        if(col.gameObject.tag == "TrapVilao")
+        {
+            Debug.Log("O selector não está colidindo com a trapVilao");
+           // MoveSelectionState.EnableSpawn = true;
+        }
+    }
 }
