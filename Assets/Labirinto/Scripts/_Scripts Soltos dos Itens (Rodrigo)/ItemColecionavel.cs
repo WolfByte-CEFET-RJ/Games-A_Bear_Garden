a5cf0971647326fbb5e1e9daa4182a9bd806773c
Rodriggs <rodrigo.cavalcante@aluno.cefet-rj.br>
2023-03-30 14:41:06 -0300

    Tarefas do Trello
    O que há de novo (Scooby-Doo)? -> criei um objeto vazio "BancoDeStats" para guardar o também novo script "PlayerStats" responsável por guardar os valores padrões para vida e ataque dos heróis e do vilão; adicionalmente criei o script "PlayerScriptGeral" como componente das unidades para importar os valores do outro script dependendo da classe do player (herói ou vilão) e também definir a sua equipe; adicionei a lógica de mana ao "ItemColecionavel" mantendo a execução apenas quando um jogador parar na casa e também adicionando valores diferentes de mana dependendo da equipe do jogador que pegá-la; por fim, readicionei o prefab do item colecionável ao projeto (perdemos esse prefab no problema de versionamento do github) e o ajustei para não ficar estranho no labirinto.
    Over and Out!

--- Assets/Labirinto/Scripts/_Scripts Soltos dos Itens (Rodrigo)/ItemColecionavel.cs
+++ Assets/Labirinto/Scripts/_Scripts Soltos dos Itens (Rodrigo)/ItemColecionavel.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class ItemColecionavel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    PlayerScriptGeral _player;   //Rodrigo --> Variável para dialogar com o script de monitoramento do jogador que efetuar a colisão (especificamente a equipe dele)
+    bool condition = true;      //Rodrigo --> Variável para restringir o aumento de mana a uma vez
+
+    // Start is called before the first frame update    
     private void OnTriggerStay2D(Collider2D col) {
         if(col.gameObject.tag == "Unit")// se o item colecionavel colidir com uma unidade e essa unidade estiver com a velocidade 0, executará um código
         {
+            _player = col.gameObject.GetComponent<PlayerScriptGeral>(); //Rodrigo --> Identifica o jogador durante a colisão        
+
             Debug.Log("Velocidade Unit: "+ col.gameObject.GetComponent<Rigidbody2D>().velocity);
             
             if(Turnos.hasMoved == true)
             {
-                Debug.Log("COLIDIUU");
+                if(_player.equipe == '0'){  //Rodrigo --> Adiciona 20 de mana se for o vilão
+                    while(condition)
+                    {
+                        _player.mana += 20;
+                        condition = false;
+                    }
+                }
+                else if(_player.equipe == '1'){ //Rodrigo --> Adiciona 10 de mana se for o herói
+                    while(condition)
+                    {
+                        _player.mana += 10;
+                        condition = false;
+                    }                
+                }
+                //Debug.Log("COLIDIUU");
                 Destroy(this.gameObject);
                 SpawnerItem.CanSpawn = true;
-            }
-            
+            }            
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D col){
+        condition = true;   //Rodrigo --> Libera a variável de condição após a colisão acaba
     }
 }
---
+++ Assets/Labirinto/Scripts/_Scripts Soltos dos Itens (Rodrigo)/PlayerStats.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{  
+    //Rodrigo --> Script para armazenar e disponibilizar stats de cada jogador
+    [HideInInspector]
+    public int _vidaVilao = 9, _vidaHeroi = 4;  //Rodrigo --> Vidas diferentes para manter a vantagem do vilão
+    [HideInInspector]
+    public int _atkVilao = 2, _atkHeroi = 1;  //Rodrigo --> Ataques diferentes para manter a vantagem do vilão
+
+    //Rodrigo --> Definindo o valor dos stats dos jogadores...
+    
+    //Rodrigo --> ...se forem heróis
+    public int StatAtkHeroi(int atk)    //Rodrigo --> método para devolver o valor padrão do ataque dos heróis
+    {
+        atk = _atkHeroi; //Heróis têm 1 de ataque [Rodrigo]
+        return atk;
+    }
+
+    public int StatHPHeroi(int hp)  //Rodrigo --> método para devolver o valor padrão da vida dos heróis
+    {
+        hp = _vidaHeroi; //Heróis têm 4 de vida [Rodrigo]
+        return hp;
+    }
+
+
+    //Rodrigo --> ...se for o vilão
+    public int StatAtkVilao(int atk)    //Rodrigo --> método para devolver o valor padrão do ataque do vilão
+    {
+        atk = _atkVilao; //Vilão tem 9 de ataque [Rodrigo]
+        return atk;
+    }
+
+    public int StatHPVilao(int hp)  //Rodrigo --> método para devolver o valor padrão da vida do vilão
+    {
+        hp = _vidaVilao; //Vilão tem 9 de vida [Rodrigo]
+        return hp;
+    }
+}
