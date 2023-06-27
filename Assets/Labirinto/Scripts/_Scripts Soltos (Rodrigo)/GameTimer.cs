4d2de7a6c965057b4670a845d7dd73c95db4702b
Rodriggs <rodrigo.cavalcante@aluno.cefet-rj.br>
2023-06-27 13:23:32 -0300

    comentários no "GameTimer"
    Tinha esquecido de adicionar os comentários no script rs

--- Assets/Labirinto/Scripts/_Scripts Soltos (Rodrigo)/GameTimer.cs
+++ Assets/Labirinto/Scripts/_Scripts Soltos (Rodrigo)/GameTimer.cs
@@ -8,7 +8,7 @@ public class GameTimer : MonoBehaviour
 {
     int minutos, segundos;
     int tempoInt, condicao; 
-    bool j; //condição para restringir as somas a uma única vez
+    bool j; //Rodrigo --> condição para restringir as somas a uma única vez
     float temporizador;
     PlayerDeath _scriptDeath;
     [SerializeField] Text tempTxt;
@@ -16,7 +16,7 @@ public class GameTimer : MonoBehaviour
 
     void Awake()
     {
-        //aqui, todas as variáveis recebem seus valores
+        //Rodrigo --> aqui, todas as variáveis recebem seus valores
         _scriptDeath = GameObject.Find("EventSystem").GetComponent<PlayerDeath>();
         minutos = 5; segundos = 0;
         temporizador = 0;
@@ -27,19 +27,19 @@ public class GameTimer : MonoBehaviour
 
     void Update()
     {
-        temporizador += Time.deltaTime; //passagem do tempo
+        temporizador += Time.deltaTime; //Rodrigo --> passagem do tempo
 
-        tempoInt = (int)temporizador;   //variável inteira do tempo
+        tempoInt = (int)temporizador;   //Rodrigo --> variável inteira do tempo
         
-        if(condicao != tempoInt){   //caso passe 1 segundo...
-            segundos -=1;   //...o tempo diminui...
-            condicao = tempoInt;    //..e a condição é atualizada
+        if(condicao != tempoInt){   //Rodrigo --> caso passe 1 segundo...
+            segundos -=1;   //Rodrigo --> ...o tempo diminui...
+            condicao = tempoInt;    //Rodrigo --> ...e a condição é atualizada
         }
         
         if(segundos == 0 && minutos == 0){
-            //não diminui o tempo
+            //Rodrigo --> não diminui mais o tempo
         }
-        else if(segundos < 0 && j){
+        else if(segundos < 0 && j){ //Rodrigo --> atualizando valores: voltar segundo para 59 e diminuir minuto
             segundos = 59;
             minutos -= 1;
             j = false;
@@ -47,12 +47,12 @@ public class GameTimer : MonoBehaviour
         else
             j = true;
 
-        if(segundos == 0 && minutos == 0){
+        if(segundos == 0 && minutos == 0){  //Rodrigo --> troca de cena
             _scriptDeath.victory = "Vilão";
             //SceneManager.LoadScene("FinalDoJogo");  //modificar para a cena real aqui
         }
 
-        if(segundos >= 10)
+        if(segundos >= 10)  //Rodrigo --> atualização do texto
             tempTxt.text = "0" + minutos.ToString() + ":" + segundos.ToString();
         else
             tempTxt.text = "0" + minutos.ToString() + ":0" + segundos.ToString();
